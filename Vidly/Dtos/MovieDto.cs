49403865f725c3e03c7a1842a335449b0124621c
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-01-18 18:55:28 +0200

    Building RESTful Services with ASP.NET Web API.

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+    }
+}
